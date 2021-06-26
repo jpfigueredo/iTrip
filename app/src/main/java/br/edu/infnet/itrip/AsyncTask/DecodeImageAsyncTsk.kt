7bde91f5a7be6ebae6190410fc72aa36413415a8
Luis <luis.barbosa@al.infnet.eu.br>
2021-06-25 21:20:08 -0300

    commit

--- app/src/main/java/br/edu/infnet/itrip/AsyncTask/DecodeImageAsyncTsk.kt
+++ app/src/main/java/br/edu/infnet/itrip/AsyncTask/DecodeImageAsyncTsk.kt
@@ -5,16 +5,23 @@ import android.graphics.BitmapFactory
 import android.os.AsyncTask
 import android.util.Base64
 import android.widget.ImageView
+import java.lang.Error
 
 class DecodeImageAsyncTsk (val photo_trip: ImageView)
     : AsyncTask<String, Void, Bitmap>()
 {
     override fun doInBackground(vararg params: String): Bitmap? {
-        val bytarray: ByteArray = Base64.decode(params[0], Base64.DEFAULT)
-        val bmimage = BitmapFactory.decodeByteArray(
-            bytarray, 0,
-            bytarray.size
-        )
+        var bmimage: Bitmap? = null
+        try {
+            val bytarray: ByteArray = Base64.decode(params[0], Base64.DEFAULT)
+            bmimage = BitmapFactory.decodeByteArray(
+                bytarray, 0,
+                bytarray.size
+            )
+        } catch (e: InterruptedException) {
+            e.printStackTrace()
+        }
+
         return bmimage
     }
 
--- app/src/main/java/br/edu/infnet/itrip/AsyncTask/EncodeImageAyncTask.kt
+++ app/src/main/java/br/edu/infnet/itrip/AsyncTask/EncodeImageAyncTask.kt
@@ -10,10 +10,18 @@ class EncodeImageAyncTask(var mBitmap: Bitmap)
     : AsyncTask<Void, Unit, String>()
 {
     override fun doInBackground(vararg void: Void?): String? {
-        val baos = ByteArrayOutputStream()
-        mBitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
-        val b: ByteArray = baos.toByteArray()
-        return Base64.encodeToString(b, Base64.DEFAULT)
+        var encodeImg: String? = null
+        try {
+            val baos = ByteArrayOutputStream()
+            mBitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+            val b: ByteArray = baos.toByteArray()
+            encodeImg = Base64.encodeToString(b, Base64.DEFAULT)
+
+        } catch (e: InterruptedException) {
+            e.printStackTrace()
+        }
+
+        return encodeImg
     }
 
     override fun onPostExecute(result: String?) {
